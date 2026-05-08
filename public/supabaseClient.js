29dd903c4dd7c305410eef842ed3eb6c8f0af244
menezespericias <menezespericias@outlook.com>
2026-05-08 07:53:40 -0300

    Corrige supabaseClient.js e erros de login

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -1,5 +1,4 @@
-// supabaseClient.js
-
+// Inicialização correta do Supabase Client
 const supabase = window.supabase.createClient(
   "https://ublelxdzwhgpblnfqlic.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibGVseGR6d2hncGJsbmZxbGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDM4MTcsImV4cCI6MjA5MzY3OTgxN30.kbJkKgDEQKXcTGrgsBuf7shGlBYLgCssHc-vgmhQbAg"
