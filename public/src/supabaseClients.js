f4135654d9c804c2c11b4f0eb62b380f3ecb2af0
Carlos Menezes <cesmenezes1683@gmail.com>
2026-07-08 18:32:20 -0300

    Delete public directory

--- public/src/supabaseClients.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://ublelxdzwhgpblnfqlic.supabase.co";
-const supabaseKey = "sb_publishable_iS0SqaOb5zplTSb4HrpYDQ_Xu2mjCPc";
-export const supabase = createClient(supabaseUrl, supabaseKey);
--- public/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// supabaseClient.js — Supabase global via ESM
-
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-window.supabase = createClient(
-  "https://ublelxdzwhgpblnfqlic.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibGVseGR6d2hncGJsbmZxbGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDM4MTcsImV4cCI6MjA5MzY3OTgxN30.kbJkKgDEQKXcTGrgsBuf7shGlBYLgCssHc-vgmhQbAg"
-);
